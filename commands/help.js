9dfb627831f8013acd2d99e4254201733af01218
Karsten Beck <s3912792@student.rmit.edu.au>
2022-03-31 16:55:52 +1000

    Setup local environment

--- commands/help.js
+++ commands/help.js
@@ -2,12 +2,13 @@ const DiscordJS = require('discord.js');
 const {MessageEmbed} = require("discord.js");
 
 module.exports = {
-    category: 'Embed',
+    category: 'Help',
     description: 'Sends an embed',
     permissions: ['ADMINISTRATOR'],
+    slash: true,
 
     callback: ({message, text}) => {
-        const embed = new MessageEmbed()
+        const help = new MessageEmbed()
             .setColor('#5cd3bf')
             .setTitle('Welcome to Quizilla')
             .setDescription("Hello World!")
